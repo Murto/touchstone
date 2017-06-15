ca8e8fd4365afa49a1fba192753a985dad02957e
Murto <murto@airmail.cc>
2017-06-15 21:22:59 +0000

    Add JSON spec strings

---
+++ src/json.hpp
@@ -0,0 +1,11 @@
+#ifndef JSON_HPP
+#define JSON_HPP
+
+#include <array>
+
+namespace json {
+	const std::string whitespace{0x20, 0x09, 0x0A, 0x0D};
+	const std::string digits{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+}
+
+#endif
