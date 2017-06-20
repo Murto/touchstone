81649359b80c60ac664c925b92435f493a5eb187
Murto <murto@airmail.cc>
2017-06-20 19:38:55 +0000

    remove redundant lines.

--- src/parsing-streams.hpp
+++ src/parsing-streams.hpp
@@ -10,9 +10,7 @@ namespace parsing {
 using namespace types;
 
 JSONNode parseJSON_stream(const std::string& str);
-JSONNode parseJSON_stream(std::string::const_iterator& start, const std::string::const_iterator end);
 JSONNode parseJSON_synced(const std::string& str);
-JSONNode parseJSON_synced(std::string::const_iterator& start, const std::string::const_iterator end);
 
 }
 
