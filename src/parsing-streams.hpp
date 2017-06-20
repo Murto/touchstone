fbf5d08f7b3e4bd37cfbf75dcadb76f6646961e9
Murto <murto@airmail.cc>
2017-06-20 21:39:47 +0000

    Implement stream parsing functions.

--- src/parsing-streams.hpp
+++ src/parsing-streams.hpp
@@ -12,6 +12,15 @@ using namespace types;
 JSONNode parseJSON_stream(const std::string& str);
 JSONNode parseJSON_synced(const std::string& str);
 
+JSONObject parseJSONObject(std::istringstream& ss);
+JSONMember parseJSONMember(std::istringstream& ss);
+JSONArray parseJSONArray(std::istringstream& ss);
+JSONString parseJSONString(std::istringstream& ss);
+JSONNumber parseJSONNumber(std::istringstream& ss);
+JSONBool parseJSONBool(std::istringstream& ss);
+void parseJSONNull(std::istringstream& ss);
+
+
 }
 
 }
