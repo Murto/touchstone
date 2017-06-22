bf36da25cfe411f207924adf3cfe539e5b17f77d
Murto <murto@airmail.cc>
2017-06-22 22:45:38 +0000

    Add file writing to large json benchmark

--- benchmarks/src/large.cpp
+++ benchmarks/src/large.cpp
@@ -4,19 +4,31 @@
 #include <fstream>
 #include <iostream>
 
+void printTimeElapsed(const std::chrono::time_point<std::chrono::steady_clock>& start, const std::chrono::time_point<std::chrono::steady_clock>& end);
+
 int main() {
 	using namespace touchstone::parsing;
-	std::ifstream fs("benchmarks/json/large.json");
+	std::ifstream ifs("benchmarks/json/large.json");
 	auto start = std::chrono::steady_clock::now();
-	parseJSON(fs);
+	JSONNode node = parseJSON(ifs);
 	auto end = std::chrono::steady_clock::now();
-	std::cout << "Time elapsed:\n";
+	std::cout << "Time elapsed for file parse:\n";
+	printTimeElapsed(start, end);
+	std::ofstream ofs("/dev/null");
+	start = std::chrono::steady_clock::now();
+	ofs << node.toString();
+	end = std::chrono::steady_clock::now();
+	std::cout << "\nTime elapsed for file write:\n";
+	printTimeElapsed(start, end);
+	std::cout << "\nPress enter to quit." << std::endl;
+	std::cin.get();
+}
+
+void printTimeElapsed(const std::chrono::time_point<std::chrono::steady_clock>& start, const std::chrono::time_point<std::chrono::steady_clock>& end) {
 	std::cout << "--Seconds elapsed:      " << std::chrono::duration_cast<std::chrono::seconds>(end - start).count() << '\n';
 	std::cout << "--Milliseconds elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << '\n';
 	std::cout << "--Microseconds elapsed: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << '\n';
 	std::cout << "--Nanoseconds elapsed:  " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << '\n';
-	std::cout << "\nPress enter to quit." << std::endl;
-	std::cin.get();
 }
 
 
